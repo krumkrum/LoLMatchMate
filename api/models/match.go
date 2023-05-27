2a7d036c7650384ee6c389bf4d24f714467bd7c4
Vladimir Kauzov <krumkrum3@gmail.com>
2023-05-28 00:56:55 +0700

    create models

--- api/models/match.go
+++ api/models/match.go
@@ -1,1 +1,58 @@
 package models
+
+// MatchInfo содержит информацию о матче
+type MatchTable struct {
+	MatchPUUID  string
+	WinningTeam string
+	Duration    int32
+}
+
+type MatchInfo struct {
+	Metadata Metadata `json:"metadata"`
+	Info     Info     `json:"info"`
+}
+
+type Metadata struct {
+	DataVersion  string   `json:"dataVersion"`
+	MatchID      string   `json:"matchId"`
+	Participants []string `json:"participants"`
+}
+
+type Info struct {
+	GameCreation       int64    `json:"gameCreation"`
+	GameDuration       int      `json:"gameDuration"`
+	GameEndTimestamp   int64    `json:"gameEndTimestamp"`
+	GameID             int      `json:"gameId"`
+	GameMode           string   `json:"gameMode"`
+	GameName           string   `json:"gameName"`
+	GameStartTimestamp int64    `json:"gameStartTimestamp"`
+	GameType           string   `json:"gameType"`
+	GameVersion        string   `json:"gameVersion"`
+	MapID              int      `json:"mapId"`
+	Participants       []string `json:"participants"`
+	PlatformID         string   `json:"platformId"`
+	QueueID            int      `json:"queueId"`
+	Teams              []Team   `json:"teams"`
+	TournamentCode     string   `json:"tournamentCode"`
+}
+
+type Team struct {
+	Bans       []interface{} `json:"bans"` // Данные о банах отсутствуют, поэтому используется пустой интерфейс
+	Objectives Objectives    `json:"objectives"`
+	TeamID     int           `json:"teamId"`
+	Win        bool          `json:"win"`
+}
+
+type Objectives struct {
+	Baron      Objective `json:"baron"`
+	Champion   Objective `json:"champion"`
+	Dragon     Objective `json:"dragon"`
+	Inhibitor  Objective `json:"inhibitor"`
+	RiftHerald Objective `json:"riftHerald"`
+	Tower      Objective `json:"tower"`
+}
+
+type Objective struct {
+	First bool `json:"first"`
+	Kills int  `json:"kills"`
+}
--- api/models/player.go
+++ api/models/player.go
@@ -1,1 +1,12 @@
 package models
+
+// PlayerInfo содержит информацию об игроке
+type PlayerInfo struct {
+	ID            string `json:"id"`
+	AccountID     string `json:"accountId"`
+	PUUID         string `json:"puuid"`
+	Name          string `json:"name"`
+	ProfileIconID int    `json:"profileIconId"`
+	RevisionDate  int64  `json:"revisionDate"`
+	SummonerLevel int    `json:"summonerLevel"`
+}
